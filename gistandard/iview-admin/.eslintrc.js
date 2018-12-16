06cbbcf63e8fa5610e362e7a25814d270ff1739b
andy19055 <393225860@qq.com>
2018-12-16 10:59:24 +0800

    'w'

--- gistandard/iview-admin/.eslintrc.js
+++ gistandard/iview-admin/.eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
     'no-undef': 'off',
-    'camelcase': 'off'
+    'camelcase': 'off',
+    'semi': ['error', 'always']
   },
   parserOptions: {
     parser: 'babel-eslint'
